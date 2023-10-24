5a31059c40fff7e98147d404f6f68127814c1293
Ravi Kumawat <145572488+1998rv@users.noreply.github.com>
2023-10-24 17:02:54 +0530

    #python#pycham#image_read#import_ cv2#image#.jpg

--- image_read.py
+++ image_read.py
@@ -3,11 +3,14 @@
 # Date :- 24-10-2023
 # Code :- To read any image in our Pc
 
-
+import os
 import cv2 # ================ import library
 
-img_0 = cv2.imread('C:\\Users\\pc\\Pictures\\Screenshots\\Screenshot 2023-10-24 111121.png') # ======== this is a path from image for in your system
+paths = os.listdir(r'C:\Users\pc\PycharmProjects\pythonProject\rv_saveimg')
+for path in paths:
 
-cv2.imshow('image',img_0) # ========================= this is only variable
-cv2.waitKey(0) # ============== cv2.waitkey(0) open image and hold it
+    img_0 = cv2.imread(fr'C:\Users\pc\PycharmProjects\pythonProject\rv_saveimg\{path}') # ======== this is a path from image for in your system
+
+    cv2.imshow('image',img_0) # ========================= this is only variable
+    cv2.waitKey(0) # ============== cv2.waitkey(0) open image and hold it
 
