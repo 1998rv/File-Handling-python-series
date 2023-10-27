db7cb36062f21bd558d154dbe3a3345ef69fe9be
Ravi Kumawat <145572488+1998rv@users.noreply.github.com>
2023-10-27 13:26:13 +0530

    #page_read#pycham#python#read

---
+++ html_fileread.py
@@ -0,0 +1,11 @@
+
+# # ================= page read function ================
+# def read_saved_page(local_path):
+#   with open(local_path, "r") as f:
+#     html = f.read()
+#   return html
+#
+# path = "/home/shivam/1.html"
+# html = read_saved_page(path)
+#
+# print(html)
