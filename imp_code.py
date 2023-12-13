0d5daff896001b3d3e96430690795a7eb8b92685
1998rv <145572488+1998rv@users.noreply.github.com>
2023-12-13 16:16:53 +0530

    add comment

--- imp_code.py
+++ imp_code.py
@@ -11,13 +11,13 @@ def circle():
     w = 20
     m = (2*math.pi)/w
     r = 200
-
+# ======================================
     while 1:
         for i in range(0, w+1):
             x = int(a+r*math.sin(m*i))
             y = int(b+r*math.cos(m*i))
             cc.mouse.move(x,y)
             sleep(1)
-
+# =======================================
 if __name__ == "__main__":
     circle()
